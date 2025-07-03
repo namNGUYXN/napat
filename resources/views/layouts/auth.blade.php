87066a587e43dff3a15388a990f3dde1f349747e
namnguyxn101 <phuongnam3235@gmail.com>
2025-07-03 17:55:17 +0700

    fix: HTML, CSS for auth pages

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -2,34 +2,38 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link href="{{ asset('css/bootstrap-5.3.3.min.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('modules/auth/css/login.css') }}">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield('title')</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link href="{{ asset('css/bootstrap-5.3.3.min.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('modules/auth/css/login.css') }}">
 
 
-    @yield('styles')
+  @yield('styles')
 </head>
+
+<style>
+  :root { --bg-auth: url('{{ asset('images/goc-san-truong-cao-thang-2.jpg') }}') }
+</style>
 
 <body>
 
-    <div class="login-overlay bg-info">
-        <div id="login-wp" class="bg-white">
+  <div class="login-overlay">
+    <div id="login-wp" class="bg-white">
 
-            @yield('form-title')
+      @yield('form-title')
 
-            @yield('form-content')
+      @yield('form-content')
 
-        </div>
     </div>
+  </div>
 
-    <script src="{{ asset('js/jquery-3.7.1.js') }}"></script>
-    <script src="{{ asset('js/bootstrap-5.3.3.bundle.min.js') }}"></script>
-    <script src="{{ asset('modules/auth/js/auth.js') }}"></script>
-    
-    @yield('scripts')
+  <script src="{{ asset('js/jquery-3.7.1.js') }}"></script>
+  <script src="{{ asset('js/bootstrap-5.3.3.bundle.min.js') }}"></script>
+  <script src="{{ asset('modules/auth/js/auth.js') }}"></script>
+
+  @yield('scripts')
 </body>
 
 </html>
