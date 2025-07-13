3a367047a48b5fec832e37df26369507b831a52f
namnguyxn101 <phuongnam3235@gmail.com>
2025-07-14 03:37:03 +0700

    fix: menu-sidebar mobile in administrator

--- resources/views/admin/partials/_menu-sidebar-mobile.blade.php
+++ resources/views/admin/partials/_menu-sidebar-mobile.blade.php
@@ -20,10 +20,10 @@
         </a>
         <ul class="submenu-sidebar custom-scrollbar">
           <li>
-            <a href="#" class="nav-link"><span class="nav-text">Xem danh sách</span></a>
+            <a href="/khoa" class="nav-link"><span class="nav-text">Xem danh sách</span></a>
           </li>
           <li>
-            <a href="#" class="nav-link"><span class="nav-text">Thêm Khoa mới</span></a>
+            <a href="{{ route('khoa.them') }}" class="nav-link"><span class="nav-text">Thêm Khoa mới</span></a>
           </li>
         </ul>
       </li>
@@ -35,10 +35,10 @@
         </a>
         <ul class="submenu-sidebar custom-scrollbar">
           <li>
-            <a href="#" class="nav-link"><span class="nav-text">Xem danh sách</span></a>
+            <a href="/nguoi-dung" class="nav-link"><span class="nav-text">Xem danh sách</span></a>
           </li>
           <li>
-            <a href="#" class="nav-link"><span class="nav-text">Thêm Người dùng mới</span></a>
+            <a href="{{ route('nguoi-dung.them') }}" class="nav-link"><span class="nav-text">Thêm Người dùng mới</span></a>
           </li>
         </ul>
       </li>
